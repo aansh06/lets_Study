51516be0157219e72ee5eb8b0d9ebfa6038e9f6f
aansh06 <95288420+aansh06@users.noreply.github.com>
2024-05-19 08:49:43 +0000

    added user authentication

--- study/views.py
+++ study/views.py
@@ -3,8 +3,39 @@ from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
+
 # Create your views here.
 
+
+def loginPage(request):
+
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        try :
+            user=User.objects.get(username=username)
+        except:
+            messages.error(request, "User Doesn't exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, ' Username or Password incorrect')
+
+    context={}
+    return render(request,'study/login_register.html',context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q= request.GET.get('q') if request.GET.get('q') != None else ''
